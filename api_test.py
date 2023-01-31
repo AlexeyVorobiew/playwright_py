64bc4d9ab24cf34919c6607483e7610fd2e12f9a
Alexey Vorobiev <@recursive_being>
2023-01-31 23:41:47 +0300

    modified:   api_test.py

--- api_test.py
+++ api_test.py
@@ -1,5 +1,7 @@
 import os
 from playwright.sync_api import sync_playwright
+from playwright.sync_api import APIRequest, expec
+'''
 
 with sync_playwright() as p:
     url = "https://dog.ceo/api/breeds/list/all"
@@ -7,3 +9,7 @@ with sync_playwright() as p:
     context = browser.new_context(base_url="https://api.github.com")
     api_request_context = context.request
     page = context.new_page()
+'''
+
+def simple_api_test(api:APIRequest):
+    APIRequest.new_context(base_url="https://playwright.dev/")
