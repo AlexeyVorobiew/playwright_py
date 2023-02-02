178e38ac8c9e64337e66cc446befbdc53c05ff07
Alexey Vorobiev <alexey.worobiew@gmail.com>
2023-02-02 23:37:29 +0300

    modified:   api_test.py

--- api_test.py
+++ api_test.py
@@ -12,12 +12,12 @@ with sync_playwright() as p:
     context = browser.new_context(base_url="https://api.github.com")
     api_request_context = context.request
     page = context.new_page()
-'''
+
 
 def test_simple_api_test():
     with playwright.http.get("https://api.example.com/data") as response:
         assert response.status_code == 200
         print(response.json())
-
+'''
 def simple_right_api_test():
     assert requests.get(url="https://api.example.com/data").status_code == 200
